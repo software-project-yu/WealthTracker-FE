0915cb44ae7b121fbb35334622d8040be398501d
tkv00 <tkv0098@gmail.com>
2024-11-06 22:07:23 +0900

    ✨feat:graph 컴포넌트 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,52 @@
+import styled from "styled-components";
+import Graph from "../components/Graph";
 import Layout from "../components/Layout";
 
 export default function Main() {
+  //예시 데이터
+  //실제 구현 시 서버API 통신으로 데이터를 받아옵니다.
+  const data = [
+    {
+      month: 11,
+      weekNum: 1,
+      thisWeekTotalCost: 20046,
+      lastWeekTotalCost: 2120,
+    },
+    {
+      month: 11,
+      weekNum: 2,
+      thisWeekTotalCost: 11110,
+      lastWeekTotalCost: 1120,
+    },
+    {
+      month: 11,
+      weekNum: 3,
+      thisWeekTotalCost: 12000,
+      lastWeekTotalCost: 30069,
+    },
+    {
+      month: 11,
+      weekNum: 4,
+      thisWeekTotalCost: 22212,
+      lastWeekTotalCost: 10022,
+    },
+    {
+      month: 11,
+      weekNum: 5,
+      thisWeekTotalCost: 12100,
+      lastWeekTotalCost: 10000,
+    },
+  ];
   return (
     <Layout>
       <h1>Example</h1>
+      <Container>
+        <Graph data={data} />
+      </Container>
     </Layout>
   );
 }
+const Container = styled.div`
+  height: 20rem;
+  width: 60rem;
+`;
