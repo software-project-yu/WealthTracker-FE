bf0ece9942ed72fd47175e2838cd20b45dd8b138
tkv00 <tkv0098@gmail.com>
2024-11-08 16:09:17 +0900

    ✨feat:반원 그래프 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Graph from "../components/Graph";
 import Layout from "../components/Layout";
+import CircleGraph from "../components/CircleGraph";
 
 export default function Main() {
   //예시 데이터
@@ -37,12 +38,23 @@ export default function Main() {
       lastWeekTotalCost: 10000,
     },
   ];
+  //원형 그래프 데이터 예시
+  const goalAmount = 100; // 목표 금액
+  const currentAmount = 85; // 현재 금액
+  const chartWidth = 260; // 원하는 가로 크기
+  const chartHeight = 120; // 원하는 높이
   return (
     <Layout>
       <h1>Example</h1>
       <Container>
         <Graph data={data} />
       </Container>
+      <CircleGraph
+        goalAmount={goalAmount}
+        currentAmount={currentAmount}
+        width={chartWidth}
+        height={chartHeight}
+      />
     </Layout>
   );
 }
