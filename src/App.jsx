f437499a8a854725f531e9a6fe1b46c7247a8a3e
Minsoo <minsoo6576@gmail.com>
2024-11-09 20:39:52 +0900

    페이지로 넘어갈 때 메인페이지의 ui 유지

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
 import Main from "./pages/Main";
 import Transactions from "./pages/Transactions";
 import ScheduledPayments from "./pages/ScheduledPayments";
 import Expense from "./pages/Expense";
 import Goals from "./pages/Goals";
 import Settings from "./pages/Settings";
-import styled from "styled-components";
 
 function App() {
   return (
-      <Routes>
+    <Routes>
+      <Route element={<Layout />}>
         <Route path="/" element={<Main />} />
         <Route path="/transactions" element={<Transactions />} />
         <Route path="/scheduledpayments" element={<ScheduledPayments />} />
         <Route path="/expenses" element={<Expense />} />
         <Route path="/goals" element={<Goals />} />
         <Route path="/settings" element={<Settings />} />
-      </Routes>
+      </Route>
+    </Routes>
   );
 }
 
