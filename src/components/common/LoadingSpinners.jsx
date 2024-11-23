50e1f65885c738ac310b54161e2991cd269d18ad
tkv00 <tkv0098@gmail.com>
2024-11-23 14:49:12 +0900

    🎨style:spinner 스타일 변경

--- src/components/common/LoadingSpinners.jsx
+++ src/components/common/LoadingSpinners.jsx
@@ -1,8 +1,8 @@
-import { GridLoader } from "react-spinners";
+import { MoonLoader } from "react-spinners";
 import { useTheme } from "styled-components";
 export default function LoadingSpinners({ size }) {
   //원하는 사이즈 입력
   const theme = useTheme();
-  const loadingSize = size || 15;
-  return <GridLoader size={loadingSize} color={theme.colors.blue} />;
+  const loadingSize = size || 50;
+  return <MoonLoader size={loadingSize} color={theme.colors.blue} />;
 }
