f2579c6de99a0f87c6f7f171514457dbcc36c583
tkv00 <tkv0098@gmail.com>
2024-11-23 14:11:28 +0900

    ✨feat:api,useFetch hook 구현

--- src/theme.js
+++ src/theme.js
@@ -14,7 +14,8 @@ const theme = {
     gray04: "#919EAB",
     gray05: "#2b2b2b",
     gray06: "#878787",
-    gray07:"#C4C4C4"
+    gray07:"#C4C4C4",
+    gray08:"#F1F1F1"
   },
 };
 export default theme;
