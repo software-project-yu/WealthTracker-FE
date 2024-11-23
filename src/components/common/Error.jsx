a5ebdbbc04830c4975f34401e8b626c46de8bd49
tkv00 <tkv0098@gmail.com>
2024-11-23 15:07:08 +0900

    ✨feat:에러 컴포넌트 구현

--- src/components/common/Error.jsx
+++ src/components/common/Error.jsx
@@ -1,4 +1,18 @@
 import { MdError } from "react-icons/md";
-export default function Error() {
-  return <MdError size={20}/>;
+import styled, { useTheme } from "styled-components";
+export default function Error({ size }) {
+  const errorSize = size || 70;
+  const theme = useTheme();
+  return (
+    <Container>
+      <MdError size={errorSize} color={theme.colors.red} />
+      <strong>잠시 후 다시 시도하세요.</strong>
+    </Container>
+  );
 }
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+`;
--- src/components/common/LoadingSpinners.jsx
+++ src/components/common/LoadingSpinners.jsx
@@ -1,8 +1,19 @@
 import { MoonLoader } from "react-spinners";
-import { useTheme } from "styled-components";
+import styled, { useTheme } from "styled-components";
 export default function LoadingSpinners({ size }) {
   //원하는 사이즈 입력
   const theme = useTheme();
   const loadingSize = size || 50;
-  return <MoonLoader size={loadingSize} color={theme.colors.blue} />;
+  return (
+    <Container>
+      <MoonLoader size={loadingSize} color={theme.colors.blue} />
+    </Container>
+  );
 }
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
